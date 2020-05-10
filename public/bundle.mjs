cbb127985bb160274b868b2b0c3ecbac70b959ef
Markus Felten <Markus.Felten@gmx.de>
2020-05-10 13:28:23 +0200

    feat: install & activate

--- public/bundle.mjs
+++ public/bundle.mjs
@@ -1,17 +1,11 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("service-worker.js", { scope: "./" })
-    .then(function (registration) {
-      document.querySelector("#status").textContent = "succeeded";
-    })
-    .catch(function (error) {
-      document.querySelector("#status").textContent = error;
-    });
-} else {
-  // Der verwendete Browser unterstützt Service Worker nicht.
-  var aElement = document.createElement("a");
-  aElement.href =
-    "http://www.chromium.org/blink/serviceworker/service-worker-faq";
-  aElement.textContent = "unavailable";
-  document.querySelector("#status").appendChild(aElement);
-}
+
+
+navigator.serviceWorker
+  .register("service-worker.js")
+  .then(registration => {
+    document.querySelector("#status").textContent = "succeeded";
+  })
+  .catch(error => {
+    document.querySelector("#status").textContent = error;
+  });
+
