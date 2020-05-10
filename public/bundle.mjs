da84f40c0f50d83707c3f3ed675dc68f09440ea7
Markus Felten <Markus.Felten@gmx.de>
2020-05-10 23:00:31 +0200

    fix: use inline module

--- public/bundle.mjs
+++ public/bundle.mjs
@@ -1,17 +1,18 @@
-navigator.serviceWorker
-  .register("service-worker.mjs")
-  .then(registration => {
+export async function initialize() {
+  try {
+    const registration = await navigator.serviceWorker.register(
+      "service-worker.mjs"
+    );
     document.querySelector("#status").textContent = "succeeded";
     sayHello();
-  })
-  .catch(error => {
-    document.querySelector("#status").textContent = error;
-  });
 
-export async function sayHello()
-{
+    return registration;
+  } catch (error) {
+    document.querySelector("#status").textContent = error;
+  }
+}
+
+export async function sayHello() {
   const response = await fetch("hello");
   document.querySelector("#hello").textContent = await response.text();
 }
-
-
